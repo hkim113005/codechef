403e8713811881a618a31a56ad4e442507cdad96
Hyungjae Kim <hyungjae1130@gmail.com>
2021-03-08 21:22:23 +0400

    2021-03-08

--- Practice/Beginner/Trace of Matrix - TRACE/main.cpp
+++ Practice/Beginner/Trace of Matrix - TRACE/main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 
+using namespace std;
+
+int N;
+int matrix[105][105];
+
+int trace(int x, int y, int n) {
+    if (x >= N || y >= N) {
+        return n;
+    }
+
+    return trace(x + 1, y + 1, n + matrix[x][y]);
+}
+
+void solve() {
+    int ans = 0;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        ans = max(ans, trace(i, 0, 0));
+    }
+
+    for (int j = 1; j < N; j++) {
+        ans = max(ans, trace(0, j, 0));
+    }
+
+    cout << ans << endl;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int T;
+
+    cin >> T;
+
+    while (T--) {
+        solve();
+    }
+
     return 0;
 }
